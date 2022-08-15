8941a91d16b57c44704c40ca5e7d24347cd96df3
NicoleVenachi <ludwing@unicauca.edu.co>
2022-08-15 18:55:28 -0500

    organizando lo del curso de 2021

---
+++ antiguo/salarios.js
@@ -0,0 +1,122 @@
+const colombia = [];
+
+colombia.push(
+  {
+    name: 'Juanita',
+    salary: 1000,
+  }
+);
+colombia.push(
+  {
+    name: 'Maria',
+    salary: 2000,
+  }
+);
+colombia.push(
+  {
+    name: 'Natalia',
+    salary: 1500,
+  }
+);
+colombia.push(
+  {
+    name: 'Aleja',
+    salary: 1800,
+  }
+);
+colombia.push(
+  {
+    name: 'Betty',
+    salary: 900,
+  }
+);
+colombia.push(
+  {
+    name: 'Carlos',
+    salary: 1100,
+  }
+);
+colombia.push(
+  {
+    name: 'Mario',
+    salary: 1300,
+  }
+);
+colombia.push(
+  {
+    name: 'Isabela',
+    salary: 2750,
+  }
+);
+colombia.push(
+  {
+    name: 'Elías',
+    salary: 1400,
+  }
+);
+colombia.push(
+  {
+    name: 'Camila',
+    salary: 2250,
+  }
+);
+colombia.push(
+  {
+    name: 'Julio',
+    salary: 2000,
+  }
+);
+colombia.push(
+  {
+    name: 'Luis',
+    salary: 2500,
+  }
+);
+colombia.push(
+  {
+    name: 'Brayam',
+    salary: 2200,
+  }
+);
+colombia.push(
+  {
+    name: 'Carol',
+    salary: 1600,
+  }
+);
+colombia.push(
+  {
+    name: 'Juli',
+    salary: 1700,
+  }
+);
+colombia.push(
+  {
+    name: 'Sebastián',
+    salary: 1950,
+  }
+);
+colombia.push(
+  {
+    name: 'Denis',
+    salary: 1200,
+  }
+);
+colombia.push(
+  {
+    name: 'Ernesto',
+    salary: 700,
+  }
+);
+colombia.push(
+  {
+    name: 'Fabiola',
+    salary: 1630,
+  }
+);
+colombia.push(
+  {
+    name: 'Gabriel',
+    salary: 2100,
+  }
+);
