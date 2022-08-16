add8d42a8a3a061d3110e8b0893fefe3f10fabb8
NicoleVenachi <ludwing@unicauca.edu.co>
2022-08-16 03:17:40 -0500

    agregue hambuguer menu interacion, clase8,html

--- 03_ForkedProject/main.js
+++ 03_ForkedProject/main.js
@@ -1,9 +1,19 @@
 // Al seleccionar el email de la nav bar, que se toggle el menu
 const navEmail = document.querySelector('.navbar-email');
-const desktopMenu = document.querySelector('.desktop-menu')
+const desktopMenu = document.querySelector('.desktop-menu');
+
+const menuHamIcon = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
 
 navEmail.addEventListener('click', toggleDesktopMenu);
+menuHamIcon.addEventListener('click', toggleMobileMenu);
 
+// Función Toggle del menú al darle click al email
 function toggleDesktopMenu(){
     desktopMenu.classList.toggle('inactive');
+}
+
+// Función Toggle del menú ppal al darle click al hamburguer icon
+function toggleMobileMenu(){
+    mobileMenu.classList.toggle('inactive');
 }
